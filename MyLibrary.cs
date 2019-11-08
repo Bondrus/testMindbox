949efa9f5839f68e9f3d5be99390fe16f7ee5b4b
Ruslan Bondarenko <51906158+Bondrus@users.noreply.github.com>
2019-11-08 19:27:48 +0200

    Add files via upload

---
+++ MyLibrary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public abstract class Count
+    {
+        public static double Sqare(double a)
+        {
+            return (Math.PI*Math.Pow(a,2));
+
+        }
+        public static double Sqare(double a,double b,double c)
+        {
+            double p = (a + b + c) / 2;
+            return (Math.Sqrt(p*(p-a)*(p-b)*(p-c)));
+
+        }
+
+    }
+}
